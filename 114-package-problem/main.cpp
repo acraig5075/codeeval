5a18ffd3db2ef729d23f046bdf0010ec8c38f5e6
Alasdair Craig <ac@acraig.za.net>
2017-07-14 15:35:08 +0200

    Added .gitignore file.

--- 114-package-problem/main.cpp
+++ 114-package-problem/main.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <cassert>
 #include <algorithm>
+#include <iomanip>
+#include <numeric>
 
 using std::vector;
 using std::string;
@@ -22,11 +24,11 @@ struct item
 auto split(const string &value, char delimiter) -> vector<string>;
 auto parse(const string &line, double &total) -> vector<item>;
 auto trim(string &str, const string &chars) -> void;
-auto greedy_knapsack(double &capacity, vector<item> &items)->vector<size_t>;
 auto output_csv(ostream &os, const vector<int> &ids) -> void;
 auto integer_weight(double weight) -> int;
 auto dynamic_knapsack(size_t W, vector<item> &items) -> vector<int>;
 auto reconstruct_solution(const vector<vector<int>> &value, size_t W, const vector<item> &items) -> vector<int>;
+auto debug_output(int total, vector<item> &items, const vector<int> &solution) -> void;
 
 
 int main(int argc, char *argv[])
@@ -38,16 +40,19 @@ int main(int argc, char *argv[])
 
 		if (fin.is_open())
 			{
+			vector<string> tests;
 			string line;
 			while (fin.good())
 				{
 				getline(fin, line);
+				tests.push_back(line);
 
 				if (!line.empty())
 					{
 					double total = 0;
 					vector<item> items = parse(line, total);
 					vector<int> package = dynamic_knapsack((size_t)integer_weight(total), items);
+					//debug_output(integer_weight(total), items, package);
 					output_csv(cout, package);
 					}
 				}
@@ -107,7 +112,7 @@ auto parse(const string &line, double &total) -> vector<item>
 		string cost   = fields[2];
 		trim(cost, "$");
 
-		if (weight < total)
+		if (weight <= total)
 			{
 			item i;
 			i.id = std::atoi(id.c_str());
@@ -145,50 +150,6 @@ auto trim(string &str, const string &chars) -> void
 	left_trim(str);
 }
 
-// the greedy knapsack algorithm
-// [ The sample data passes, but the online submission fails.]
-auto greedy_knapsack(double &capacity, vector<item> &items) -> vector<size_t>
-{
-	//std::for_each(begin(items), end(items), [](item & i)
-	//	{
-	//	i.ratio = i.value / i.weight;
-	//	});
-
-	std::sort(begin(items), end(items), [](const item & a, const item & b)
-		{
-		if (a.value == b.value)
-			return a.weight < b.weight;
-		else
-			return a.value > b.value;
-		});
-
-	double value = 0.0;
-	vector<size_t> knapsack;
-
-	auto itr = begin(items);
-
-	while (capacity > 0 && itr != end(items))
-		{
-		if (itr->weight <= capacity)
-			{
-			// found something that fits, so add to the knapsack
-			value += itr->value;
-			capacity -= itr->weight;
-			knapsack.push_back(itr->id);
-			}
-		//else
-		//	{
-		//	// nothing fits, so add a fraction of the most valuable
-		//	value += itr->ratio * capacity;
-		//	capacity = 0;
-		//	}
-
-		++itr;
-		}
-
-	return knapsack;
-}
-
 // output a comma separated values list
 auto output_csv(ostream &os, const vector<int> &ids) -> void
 {
@@ -222,13 +183,13 @@ auto integer_weight(double weight) -> int
 auto dynamic_knapsack(size_t W, vector<item> &items) -> vector<int>
 {
 	// sorting isn't supposed to be necessary, but doing so satisfies the sample data
-	std::sort(begin(items), end(items), [](const item &a, const item &b) 
-	{
+	std::sort(begin(items), end(items), [](const item & a, const item & b)
+		{
 		if (a.value == b.value)
 			return a.weight < b.weight;
 		else
 			return a.value > b.value;
-	});
+		});
 
 	size_t n = items.size();
 	size_t rows = items.size() + 1;
@@ -246,60 +207,60 @@ auto dynamic_knapsack(size_t W, vector<item> &items) -> vector<int>
 
 	// knapsack algorithm
 	for (size_t i = 1; i <= n; ++i)
-	{
-		for (size_t j = 1; j <= W; ++j)
 		{
+		for (size_t j = 1; j <= W; ++j)
+			{
 			int item_weight = items[i - 1].weight;
 			int item_value = items[i - 1].value;
 
 			value[i][j] = value[i - 1][j];
 			if ((size_t)(item_weight) <= j)
-			{
+				{
 				int val = value[i - 1][j - item_weight] + item_value;
 				if (value[i][j] < val)
 					value[i][j] = val;
+				}
 			}
 		}
-	}
 
 	return reconstruct_solution(value, W, items);
 }
 
-// build the solution from the dtnamic-programming 2d array
+// build the solution from the dynamic-programming 2d array
 auto reconstruct_solution(const vector<vector<int>> &value, size_t W, const vector<item> &items) -> vector<int>
 {
 	size_t r = value.size() - 1;
 	size_t c = value[0].size() - 1;
 
-	std::vector<bool> solution(r); // true if item was packed in the knapsack, false if not. 
+	std::vector<bool> solution(r); // true if item was packed in the knapsack, false if not.
 
 	while (r > 0)
-	{
+		{
 		bool item_used = false;
 
 		int item_weight = items[r - 1].weight;
 		int item_value = items[r - 1].value;
 
-		if (value[r][c] >= item_value && c > (size_t)item_weight)
-		{
+		if (value[r][c] >= item_value && c >= (size_t)item_weight)
+			{
 			int a = value[r - 1][c - item_weight];
 			int b = value[r - 1][c];
 			item_used = (a + item_value > b);
-		}
+			}
 
 		if (item_used)
-		{
+			{
 			solution[r - 1] = true;
 			c = c - item_weight;
 			r--;
-		}
+			}
 		else
-		{
+			{
 			solution[r - 1] = false;
 			c = c;
 			r--;
+			}
 		}
-	}
 
 	// go through solution and sum the knapsack contents
 	int knapsack_weight = 0;
@@ -307,14 +268,14 @@ auto reconstruct_solution(const vector<vector<int>> &value, size_t W, const vect
 	vector<int> ids;
 
 	for (size_t i = 0; i < solution.size(); ++i)
-	{
-		if (solution[i])
 		{
+		if (solution[i])
+			{
 			ids.push_back(items[i].id);
 			knapsack_weight += items[i].weight;
 			knapsack_value += items[i].value;
+			}
 		}
-	}
 
 	// global check
 	r = value.size() - 1;
@@ -322,6 +283,62 @@ auto reconstruct_solution(const vector<vector<int>> &value, size_t W, const vect
 	assert((size_t)knapsack_weight <= W);
 	assert(knapsack_value == value[r][c]);
 
+	sort(begin(ids), end(ids));
 	return ids;
+}
+
+auto debug_output(int total, vector<item> &items, const vector<int> &solution) -> void
+{
+	cout << '\n';
+
+	bool equal_cost = false;
+
+	sort(begin(items), end(items), [&equal_cost](const item & a, const item & b)
+		{
+		if (a.value == b.value)
+			{
+			equal_cost = true;
+			return a.weight < b.weight;
+			}
+		else
+			{
+			return a.value > b.value;
+			}
+		});
+
+	if (equal_cost)
+		cout << "* ";
+	else
+		cout << "  ";
+
+	for (const auto &i : items)
+		{
+		cout << i.id << ": " << i.weight << " $" << i.value << "  |  ";
+		}
+	cout << '\n';
+
+	auto pivot = std::partition(begin(items), end(items), [&solution](const item &i)
+		{
+		return std::find_if(begin(solution), end(solution), [id = i.id](int s)
+			{
+			return id == s;
+			}) != end(solution);
+		});
+
+	int used_weight = std::accumulate(begin(items), pivot, 0, [](int value, const item &i) 
+		{
+		return value + i.weight;
+		});
+
+	int unused_weight = std::accumulate(pivot, end(items), 0, [](int value, const item &i)
+		{
+		return value + i.weight;
+		});
+
+	int remaining = total - used_weight;
+
+	assert(used_weight <= total);
+
+	cout << "  " << used_weight << " < " << total << " (" << remaining << ")\n";
 }
 
