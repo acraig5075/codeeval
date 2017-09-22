53cc3e827755ac0ce43b60c4ace3eb944005b926
Alasdair Craig <ac@acraig.za.net>
2017-09-22 16:33:31 +0200

    Further work on challenge #175.

--- 175-the-cubes/main.cpp
+++ 175-the-cubes/main.cpp
@@ -1,7 +1,6 @@
 // 175-the-cubes.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
 #include <iostream>
 #include <string>
 #include <vector>
@@ -13,8 +12,14 @@ using std::string;
 using std::vector;
 using std::cout;
 using std::ifstream;
+using std::pair;
 
-auto split(const string &value, char delimiter)->vector<string>;
+using edges = vector<pair<size_t, size_t>>;
+
+auto split(const string &value, char delimiter) -> vector<string>;
+auto increment(size_t &value) -> void;
+auto decrement(size_t &value) -> void;
+
 
 class floor_plan
 {
@@ -22,7 +27,7 @@ public:
 	floor_plan(size_t d)
 		: dim(d)
 	{
-		rows.resize(d, vector<char>(d, ' '));
+		grid.resize(d, vector<char>(d, ' '));
 	}
 
 	void make(const string &s)
@@ -33,21 +38,109 @@ public:
 		size_t c = 0;
 
 		for (auto v : s)
-		{
-			rows[r][c] = v;
+			{
+			grid[r][c] = v;
 
 			c++;
 			if (c == dim)
-			{
+				{
 				r++;
 				c = 0;
+				}
 			}
-		}
 	}
+
+	size_t id(size_t level, size_t r, size_t c) const
+	{
+		return (level * dim * dim) + (r * dim) + c;
+	}
+
+	edges possible_steps(size_t level)
+	{
+		for (size_t r = 1; r < dim - 1; ++r)
+			{
+			for (size_t c = 1; c < dim - 1; ++c)
+				{
+				if (grid[r][c] != '*')
+					{
+					up(level, r, c);
+					down(level, r, c);
+					left(level, r, c);
+					right(level, r, c);
+					hole(level, r, c);
+					}
+				}
+			}
+
+		return steps;
+	}
+
+	void movement(size_t level, size_t r, size_t c, void(*rowOp)(size_t &), void(*colOp)(size_t &))
+	{
+		size_t from = id(level, r, c);
+
+		if (rowOp)
+			(*rowOp)(r);
+		if (colOp)
+			(*colOp)(c);
+
+		if (grid[r][c] == '*')
+			return;
+
+		steps.push_back({ from, id(level, r, c) });
+
+		// handle jump over hole
+		if (grid[r][c] == 'o')
+			{
+			if (rowOp)
+				(*rowOp)(r);
+			if (colOp)
+				(*colOp)(c);
+
+			if (grid[r][c] != '*')
+				steps.push_back({ from, id(level, r, c) });
+			}
+	}
+
+	void up(size_t level, size_t r, size_t c)
+	{
+		movement(level, r, c, &decrement, nullptr);
+	}
+
+	void down(size_t level, size_t r, size_t c)
+	{
+		movement(level, r, c, &increment, nullptr);
+	}
+
+	void left(size_t level, size_t r, size_t c)
+	{
+		movement(level, r, c, nullptr, &decrement);
+	}
+
+	void right(size_t level, size_t r, size_t c)
+	{
+		movement(level, r, c, nullptr, &increment);
+	}
+
+	void hole(size_t level, size_t r, size_t c)
+	{
+		assert(level > 0);
+
+		if (grid[r][c] == 'o')
+			{
+			size_t from = id(level, r, c);
+			size_t to = id(level - 1, r, c);
+
+			steps.push_back({ from, to });
+			steps.push_back({ to, from });
+			}
+	}
+
 
 private:
 	size_t dim = 0;
-	vector<vector<char>> rows;
+	vector<vector<char>> grid;
+	edges steps;
 };
 
 class labyrinth
@@ -62,10 +155,30 @@ public:
 	void make(const string &s)
 	{
 		for (size_t i = 0; i < dim; ++i)
-		{
+			{
 			size_t start = i * dim;
 			levels[i].make(s.substr(start, dim * dim));
-		}
+			}
+	}
+
+	edges all_steps()
+	{
+		edges all;
+
+		for (size_t i = 0; i < dim; ++i)
+			{
+			edges steps = levels[i].possible_steps(i);
+			all.insert(end(all), begin(steps), end(steps));
+			}
+
+		// todo add entrance and exit step
+
+		return all;
+	}
+
+	size_t level_opening(size_t level)
+	{
+		return 0;
 	}
 
 private:
@@ -76,19 +189,19 @@ private:
 int main(int argc, char *argv[])
 {
 	if (argc > 1)
-	{
+		{
 		string filename(argv[1]);
 		ifstream fin(filename.c_str());
 
 		if (fin.is_open())
-		{
+			{
 			string line;
 			while (fin.good())
-			{
+				{
 				getline(fin, line);
 
 				if (!line.empty())
-				{
+					{
 					auto tokens = split(line, ';');
 					auto str1 = tokens[0];
 					auto str2 = tokens[1];
@@ -98,10 +211,19 @@ int main(int argc, char *argv[])
 
 					labyrinth labyrinth(dim);
 					labyrinth.make(str2);
+
+					edges movements = labyrinth.all_steps();
+					size_t start = labyrinth.level_opening(0);
+					size_t end = labyrinth.level_opening(dim - 1);
+
+					//graph g(dim * dim * dim);
+					//g.read(movements);
+					//g.bfs(start);
+					//g.get_distance(end);
+					}
 				}
 			}
 		}
-	}
 
 	return 0;
 }
@@ -113,20 +235,29 @@ auto split(const string &value, char delimiter) -> vector<string>
 
 	size_t start = 0;
 	while (true)
-	{
+		{
 		size_t pos = value.find(delimiter, start);
 		if (pos == string::npos)
-		{
+			{
 			ret.push_back(value.substr(start));
 			break;
-		}
+			}
 		else
-		{
+			{
 			ret.push_back(value.substr(start, pos - start));
 			start = pos + 1;
+			}
 		}
-	}
 
 	return ret;
 }
 
+auto increment(size_t &value) -> void
+{
+	++value;
+}
+
+auto decrement(size_t &value) -> void
+{
+	--value;
+}
